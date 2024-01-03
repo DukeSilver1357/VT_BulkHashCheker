72e9d6eb5b764a335237d7b75f5df31718c1af5f
Jonny Elrod <jelrod@reliaquest.com>
2024-01-03 08:38:56 -0500

    Add pandas library and command line args.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # VT_BulkHashCheker
 This is a basic python script which can be used for static analysis of suspicious files in bulk. It utilises VirusTotal API V3 for checking the hashes. 
 The VT_BulkHashChecker creates a table out of API's json output and writes it down to a file of your choice. 
-Hashes can be fed via another csv or txt file. Due to 4 lookups/min limitation, there is a 20 seconds sleep command in between checking hashes.  
+Hashes can be fed via another csv or txt file.
+
+## Example Usage
+`python3 VT_Bulkhashchecker.py my_key samplehashes.txt sampleresults.xlsx`
+
 
 ## Virus Total API
 For more information about Virus Total API please visit https://developers.virustotal.com/reference/overview. For this script you will need a Virus Total standard free public API key.
--- VT_Bulkhashchecker.py
+++ VT_Bulkhashchecker.py
@@ -1,31 +1,45 @@
 import requests
-import json
 import time
+import sys
+import pandas as pd
+from tqdm import tqdm
+import urllib3
+urllib3.disable_warnings()
 
-apikey = '<YOUR API KEY>'
-hashes = open("/input/file/here") # the hashes to check
-analysis = open("/output/file/here", "w") # the file to save to the result
-analysis.write("\t\t\tLink,File Name,File Type,Undetected,Detected_Suspicious,Detected_Malicious,Threat Label,Tag1,Tag2,Tag3,Tag4"
-               "\n")
-for hashn in hashes:
-        print('Checking hash ' + hashn)
+
+def vt_bulk_hash_check(apikey: str, hashes: pd.DataFrame) -> pd.DataFrame:
+    pbar = tqdm(total=len(hashes.columns), desc="Bulk lookup started...")
+    hash_results = {}
+
+    for hashn in hashes:
+        pbar.display()
         url = "https://www.virustotal.com/api/v3/files/"
-        VTlink= "https://www.virustotal.com/gui/file/"
         headers = {
-             "accept": "application/json",
-             "x-apikey": "<YOUR API KEY>"
-             }
-        response= requests.get(url+hashn, headers=headers, timeout= 120)
-       
-        if response.status_code == 404:
-          result = response.json()         
-          analysis.write(VTlink+hashn.strip() + ","+ "Not Found in Virus Total Database"+"\n")
+            "accept": "application/json",
+            "x-apikey": apikey
+        }
+        response = requests.get(url + hashn, headers=headers, timeout=120, verify=False)
+        pbar.update(1)
+        if response.status_code != 200:
+            hash_results[hashn] = {}
+            hash_results[hashn]['vt_results'] = False
+        else:
+            result = response.json()
+            hash_results[hashn] = {}
+            hash_results[hashn]['vt_results'] = True
+            hash_results[hashn]['file_name'] = result['data']['attributes']['names'][0]
+            hash_results[hashn]['file_type'] = result['data']['attributes']['type_description']
+            hash_results[hashn]['detected_suspicious'] = result['data']['attributes']['last_analysis_stats'][
+                'suspicious']
+            hash_results[hashn]['detected_malicious'] = result['data']['attributes']['last_analysis_stats']['malicious']
+            hash_results[hashn]['threat_label'] = result['data']['attributes']['popular_threat_classification'][
+                'suggested_threat_label']
+            hash_results[hashn]['tags'] = str(result['data']['attributes']['tags'])
+        time.sleep(1)
+    return pd.DataFrame.from_dict(hash_results, orient='index')
 
-        elif response.status_code == 200:
-         result = response.json()
-# write only the files recognized as malicious
-         analysis.write((((((((VTlink+hashn.strip() + "," + str(result['data']['attributes']['names'][0])+ ",") +str(result['data']['attributes']['type_description'])+",") + str(result['data']['attributes']['last_analysis_stats']['undetected'])+",")
-                   +str(result['data']['attributes']['last_analysis_stats']['suspicious'])+",")+str(result['data']['attributes']['last_analysis_stats']['malicious'])+",")
-                   + str(result['data']['attributes']['popular_threat_classification']['suggested_threat_label'])+",")
-                   +str(result['data']['attributes']['tags'])+",")+"\n")
-        time.sleep(1 * 20)
+
+if __name__ == "__main__":
+    results_df = vt_bulk_hash_check(apikey=sys.argv[1],
+                                    hashes=pd.read_csv(sys.argv[2]))
+    results_df.to_excel(sys.argv[3])
